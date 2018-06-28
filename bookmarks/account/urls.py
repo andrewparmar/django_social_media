7779b957392ab4e1ff382cc642fd84bc4b53c63b
Andrew Parmar <andrew@sendhub.com>
2018-06-28 15:40:49 -0700

    Added password reset functionality

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,15 +1,28 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
+
 
 urlpatterns = [
 
     url(r'^$', views.dashboard, name='dashboard'),
 
     #login/logout views
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
-    url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
     url(r'^jello/$', views.jello_world, name='jello'),
+
+
+    #chang password urls
+    url(r'^password-change/$', auth_views.password_change, name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
+
+    #password reset
+    url(r'^password-reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
     # Old route
     # url(r'^login/$', views.user_login, name='login'),
